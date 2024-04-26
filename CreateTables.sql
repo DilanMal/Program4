9d5faa20f4fdfa5d88a89fec0b2cb5e322cfc072
AdityaGupta <82144136+adityagupta8121@users.noreply.github.com>
2024-04-26 15:07:01 -0700

    fixed formatting for CreateTables.sql

--- CreateTables.sql
+++ CreateTables.sql
@@ -1,35 +1,62 @@
-CREATE TABLE Membership ("MemberID" int NOT NULL PRIMARY KEY, "Name" varChar2(50) NOT NULL, "TeleNum" int NOT NULL, 
-"Addr" varChar2(255), "TotalSpent" int, "LastDate" DATE, "Tickets" int, "Tokens" int);
+SET autocommit off;
+SET pagesize 500
+SET linesize 500
+SET define off
 
-CREATE TABLE Game ("GameID" int NOT NULL PRIMARY KEY, "GName" varChar2(255), "TokenCost" int);
+CREATE TABLE Membership (
+    MemberID int NOT NULL PRIMARY KEY, 
+    Name varChar2(50) NOT NULL, 
+    TeleNum int NOT NULL, 
+    Addr varChar2(255), 
+    TotalSpend int, 
+    LastDate DATE, 
+    Tickets int, 
+    Tokens int);
 
-CREATE TABLE GameXact ("GXactID" int NOT NULL PRIMARY KEY, "MemberID" int NOT NULL, "GameID" int NOT NULL, "Score" int);
+CREATE TABLE Game (
+    GameID int NOT NULL PRIMARY KEY, 
+    GName varChar2(255), 
+    TokenCost int);
 
-CREATE TABLE TTXact ("TTXactID" int NOT NULL PRIMARY KEY, "MemberID" int NOT NULL, "Category" varChar2(255), 
-"Amount" int NOT NULL, "Date" DATE);
+CREATE TABLE GameXact (
+    GXactID int NOT NULL PRIMARY KEY, 
+    MemberID int NOT NULL, 
+    GameID int NOT NULL, 
+    Score int);
 
-CREATE TABLE PrizeXact ("PXactID" int NOT NULL PRIMARY KEY, "MemberID" int NOT NULL, "PrizeID" int NOT NULL);
+CREATE TABLE TTXact (
+    TTXactID int NOT NULL PRIMARY KEY, 
+    MemberID int NOT NULL, 
+    Category varChar2(255), 
+    Amount int NOT NULL, 
+    Date DATE);
 
-CREATE TABLE Prize ("PrizeID" int NOT NULL PRIMARY KEY, "PName" varChar2(255), "TicketCost" int);
+CREATE TABLE PrizeXact (
+    PXactID int NOT NULL PRIMARY KEY, 
+    MemberID int NOT NULL, 
+    PrizeID int NOT NULL);
 
-CREATE TABLE CouponXact ("CXactID" int NOT NULL PRIMARY KEY, "MemberID" int NOT NULL, "CoupID" int NOT NULL);
+CREATE TABLE Prize (
+    PrizeID int NOT NULL PRIMARY KEY, 
+    PName varChar2(255), 
+    TicketCost int);
 
-CREATE TABLE Coupon ("CoupID" int NOT NULL PRIMARY KEY, "FoodItem" varchar2(255));
+CREATE TABLE CouponXact (
+    CXactID int NOT NULL PRIMARY KEY, 
+    MemberID int NOT NULL, 
+    CoupID int NOT NULL);
+
+CREATE TABLE Coupon (
+    CoupID int NOT NULL PRIMARY KEY, 
+    FoodItem varchar2(255));
 
 GRANT SELECT, DELETE, INSERT, UPDATE ON Membership TO PUBLIC;
-
 GRANT SELECT, DELETE, INSERT, UPDATE ON Game TO PUBLIC;
-
 GRANT SELECT, DELETE, INSERT, UPDATE ON GameXact TO PUBLIC;
-
 GRANT SELECT, DELETE, INSERT, UPDATE ON TTXact TO PUBLIC;
-
 GRANT SELECT, DELETE, INSERT, UPDATE ON PrizeXact TO PUBLIC;
-
 GRANT SELECT, DELETE, INSERT, UPDATE ON Prize TO PUBLIC;
-
 GRANT SELECT, DELETE, INSERT, UPDATE ON Coupon TO PUBLIC;
-
 GRANT SELECT, DELETE, INSERT, UPDATE ON CouponXact TO PUBLIC;
 
 Commit;
